6b1226f5b545dfff9e142272b0f93328f6d6d592
Hernan <villahrnn@hotmail.com>
2022-01-16 20:47:42 -0300

    respeta orden

--- src/com/company/Tournament.java
+++ src/com/company/Tournament.java
@@ -6,6 +6,7 @@ public class Tournament {
     private int noPlayers;
     private List<Integer> playersOrder;
     private List<String> players;
+    private boolean firstPrintFlag = true;
 
     public Tournament() {
         this.playersOrder = new ArrayList<>();
@@ -15,6 +16,16 @@ public class Tournament {
     private List<Integer> getPlayersOrder() {
         playersOrder = getRandomList(noPlayers);
         return playersOrder;
+    }
+
+    private void organizePlayers(){
+        playersOrder = getPlayersOrder();
+        List<String> aux = new ArrayList<>();
+        for (int i = 0; i < players.size(); i++){
+            aux.add(players.get(playersOrder.get(i)));
+        }
+        players.clear();
+        players.addAll(aux);
     }
 
     private List<Integer> getRandomList(int players) {
@@ -42,12 +53,12 @@ public class Tournament {
     }
 
     public void printPlayers(){
-        playersOrder = getPlayersOrder();
         int x = 0;
+        if (firstPrintFlag) organizePlayers();
         if (noPlayers != 1) {
             for (int i = 0; i < noPlayers / 2; i++) {
                 for (int z = 0; z < 2; z++) {
-                    String player = players.get(playersOrder.get(x));
+                    String player = players.get(x);
                     if (!player.isBlank()) {
                         System.out.printf(player + "\n");
                     }
@@ -55,8 +66,9 @@ public class Tournament {
                 }
                 System.out.println();
             }
+            firstPrintFlag = false;
         } else {
-            System.out.println("Winner of tournament is: " + players.get(0));
+                System.out.println("Winner of tournament is: " + players.get(0));
         }
     }
 }
